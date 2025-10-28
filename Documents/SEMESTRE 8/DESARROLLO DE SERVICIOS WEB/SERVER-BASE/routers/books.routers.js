2a72eccd97acb6b09022198f8b335dcce6bc6542
jolumart20 <jolumartinezro@ittepic.edu.mx>
2025-10-27 20:38:20 -0700

    x

--- Documents/SEMESTRE 8/DESARROLLO DE SERVICIOS WEB/SERVER-BASE/routers/books.routers.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const booksController = require('../controllers/books_controller.js');
-router.get('/', booksController.getBooks);
-router.get('/:bookId', booksController.getBookById);
-router.post('/', booksController.createBook);
-router.put('/:bookId', booksController.updateBook);
-router.delete('/:bookId', booksController.deleteBook);
-module.exports = router;
--- Documents/SEMESTRE 8/DESARROLLO DE SERVICIOS WEB/SERVER-BASE/utils/mongoConnection.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://jolumartinezro:bnkGBX4djEomc2eM@books.uj0uxtb.mongodb.net/?retryWrites=true&w=majority&appName=books')
-.then(() => {console.log('MongoDB connected successfully');})
-.catch((err) => {console.log('MongoDB connection error:', err);});
-
-module.exports= mongoose;
